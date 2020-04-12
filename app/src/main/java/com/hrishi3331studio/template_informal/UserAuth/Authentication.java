685d3f1133e35bf170509abffe009929577ee2a3
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2020-04-12 12:50:12 +0530

    Login/Signup FrontEnd

--- app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/Authentication.java
+++ app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/Authentication.java
@@ -1,7 +1,11 @@
-package com.hrishi3331studio.template_informal;
+package com.hrishi3331studio.template_informal.UserAuth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.hrishi3331studio.template_informal.R;
 
 public class Authentication extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class Authentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+    }
+
+    public void SignUp(View view){
+        Intent intent = new Intent(Authentication.this, SignUp.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/SignUp.java
@@ -0,0 +1,15 @@
+package com.hrishi3331studio.template_informal.UserAuth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hrishi3331studio.template_informal.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
