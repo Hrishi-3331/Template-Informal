35f0a25353e51edec0bbf397ecc84c77dbaf8653
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2020-04-12 12:16:04 +0530

    Splash Screen Backend

--- app/src/main/java/com/hrishi3331studio/template_informal/Splash.java
+++ app/src/main/java/com/hrishi3331studio/template_informal/Splash.java
@@ -1,13 +1,42 @@
 package com.hrishi3331studio.template_informal;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Splash extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent;
+                if (mUser == null){
+                    intent = new Intent(Splash.this, Authentication.class);
+                }
+                else {
+                    intent = new Intent(Splash.this, MainActivity.class);
+                }
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.postDelayed(runnable, 1500);
     }
 }
