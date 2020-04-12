cea3e3ba4ac09fd188892cd1244dbc6f72efee5e
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2020-04-12 14:03:52 +0530

    Login/Signup backend

--- app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/Authentication.java
+++ app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/Authentication.java
@@ -1,18 +1,125 @@
 package com.hrishi3331studio.template_informal.UserAuth;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hrishi3331studio.template_informal.MainActivity;
 import com.hrishi3331studio.template_informal.R;
 
 public class Authentication extends AppCompatActivity {
+
+    private FirebaseAuth jAuth;
+    private EditText user_email;
+    private EditText user_password;
+    private CoordinatorLayout cord;
+    private ProgressDialog jDialog;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        cord = (CoordinatorLayout)findViewById(R.id.cord);
+
+        user_email = (EditText)findViewById(R.id.user_email);
+        user_password = (EditText)findViewById(R.id.user_password);
+
+        jDialog = new ProgressDialog(Authentication.this);
+        jDialog.setTitle("Signing In");
+        jDialog.setMessage("Please wait...");
+        jDialog.setCancelable(false);
+        jDialog.setCanceledOnTouchOutside(false);
+
+        jAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void Login(View view){
+        String Email = user_email.getText().toString();
+        String Password = user_password.getText().toString();
+
+        if (Email.isEmpty()){
+            Snackbar snackbar = Snackbar.make(cord, "Please enter Email ID", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+        else if (Password.isEmpty()){
+            Snackbar snackbar = Snackbar.make(cord, "Please enter Password", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+        else {
+            jDialog.show();
+            jAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    jDialog.dismiss();
+                    if (task.isSuccessful()){
+                        mUser = jAuth.getCurrentUser();
+                        Intent intent = new Intent(Authentication.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else {
+                        Snackbar snackbar = Snackbar.make(cord, "Invalid credentials. Please try again!", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                }
+            });
+        }
+    }
+
+    public void ResetPassword(View view){
+        final String Email = user_email.getText().toString();
+        if (Email.isEmpty()){
+            Snackbar snackbar = Snackbar.make(cord, "Please enter Email ID to reset password", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+        else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Authentication.this);
+            builder.setTitle("Reset Password")
+                    .setMessage("Do you want to send password reset link?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(final DialogInterface dialog, int which) {
+                            jAuth.sendPasswordResetEmail(Email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    dialog.dismiss();
+                                    if (task.isSuccessful()){
+                                        Snackbar snackbar = Snackbar.make(cord, "Password reset link sent to your email Id", Snackbar.LENGTH_LONG);
+                                        snackbar.show();
+                                    }
+                                    else {
+                                        Snackbar snackbar = Snackbar.make(cord, "Error in resetting password. Please try again!", Snackbar.LENGTH_LONG);
+                                        snackbar.show();
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 
     public void SignUp(View view){
--- app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/SignUp.java
+++ app/src/main/java/com/hrishi3331studio/template_informal/UserAuth/SignUp.java
@@ -1,15 +1,145 @@
 package com.hrishi3331studio.template_informal.UserAuth;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hrishi3331studio.template_informal.MainActivity;
 import com.hrishi3331studio.template_informal.R;
 
 public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRef;
+    private EditText name;
+    private EditText email;
+    private EditText mobile;
+    private EditText password;
+    private EditText password2;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        mRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        name = (EditText)findViewById(R.id.form_name);
+        email = (EditText)findViewById(R.id.form_email);
+        mobile = (EditText)findViewById(R.id.form_phone);
+        password = (EditText)findViewById(R.id.form_pass);
+        password2 = (EditText)findViewById(R.id.form_pass2);
+
+        dialog = new ProgressDialog(SignUp.this);
+        dialog.setTitle("Creating Account");
+        dialog.setMessage("Please wait...");
+        dialog.setCanceledOnTouchOutside(false);
+    }
+
+    public void Register(View view){
+        String name = this.name.getText().toString();
+        String email = this.email.getText().toString();
+        String mobile = this.mobile.getText().toString();
+        String password = this.password.getText().toString();
+        String password2 = this.password2.getText().toString();
+
+        if (name.isEmpty()){
+            Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+        }
+        else if (email.isEmpty()){
+            Toast.makeText(this, "Please enter a valid Email ID", Toast.LENGTH_SHORT).show();
+        }
+        else if (mobile.isEmpty()){
+            Toast.makeText(this, "Please enter your mobile number", Toast.LENGTH_SHORT).show();
+        }
+        else if (password.length() < 6){
+            Toast.makeText(this, "Password length should be atleast 6 characters", Toast.LENGTH_SHORT).show();
+        }
+        else if (!password.equals(password2)){
+            Toast.makeText(this, "Re-entered password did not match", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ConfirmSignUp(name, email, password, mobile);
+        }
+    }
+
+    private void ConfirmSignUp(final String name, final String email, final String password, final String mobile) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+        builder.setTitle("Sign Up");
+        builder.setMessage("Do you want to create account?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SignUp(name, email, password, mobile);
+                dialog.dismiss();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog confirmation = builder.create();
+        confirmation.show();
+    }
+
+    private void SignUp(final String name, String email, String password, final String mobile) {
+        dialog.show();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    UserProfileChangeRequest.Builder builder = new UserProfileChangeRequest.Builder();
+                    builder.setDisplayName(name);
+                    user.updateProfile(builder.build());
+                    DatabaseReference aref = mRef.child(user.getUid());
+                    aref.child("name").setValue(name);
+                    aref.child("contact").setValue(mobile);
+                    aref.child("id").setValue(user.getUid());
+                    aref.child("email").setValue(user.getEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(SignUp.this, "New Account created successfully", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                                startActivity(new Intent(SignUp.this, MainActivity.class));
+                                finish();
+                            }else {
+                                Toast.makeText(SignUp.this, "Error in updating your account. Please try again or contact us", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+
+                }
+                else {
+                    Toast.makeText(SignUp.this, task.getException().getLocalizedMessage().trim(), Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            }
+        });
+
+    }
+
+    public void goBack(View view){
+        finish();
     }
 }
