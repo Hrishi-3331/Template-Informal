cfac9389d47ff2bec105223538bcc999f595ae4b
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2020-04-12 11:53:37 +0530

    Setup Project

---
+++ app/src/main/java/com/hrishi3331studio/template_informal/Splash.java
@@ -0,0 +1,13 @@
+package com.hrishi3331studio.template_informal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
